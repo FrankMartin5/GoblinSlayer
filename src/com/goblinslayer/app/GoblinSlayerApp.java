d19d5acd0d0826b815c023e03901e93963c54a72
GroovyCj <hairston917@gmail.com>
2022-05-16 15:38:36 -0600

    Added createPlayer method

---
+++ src/com/goblinslayer/app/GoblinSlayerApp.java
@@ -0,0 +1,49 @@
+package com.goblinslayer.app;
+
+import com.goblinslayer.character.Player;
+import com.goblinslayer.character.Weapon;
+
+import java.util.Scanner;
+
+public class GoblinSlayerApp {
+    Player p1 = new Player();
+    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        GoblinSlayerApp app = new GoblinSlayerApp();
+        app.createPlayer();
+    }
+
+    public void createPlayer(){
+        boolean badWeapon = true;
+        String name;
+        System.out.println("Please enter your Name...");
+        name = scanner.nextLine();
+        while (badWeapon) {
+            Weapon weapon = null;
+            System.out.println("What is your desired Weapon... [1]Axe, [2]Sword, [3] Staff, [4] Club");
+            String selection = scanner.next();
+            switch (selection) {
+                case ("1"):
+                    p1.setWeapon(Weapon.AXE);
+                    badWeapon = false;
+                    break;
+                case ("2"):
+                    p1.setWeapon(Weapon.SWORD);
+                    badWeapon = false;
+                    break;
+                case ("3"):
+                    p1.setWeapon(Weapon.STAFF);
+                    badWeapon = false;
+                    break;
+                case ("4"):
+                    p1.setWeapon(Weapon.CLUB);
+                    badWeapon = false;
+                    break;
+                default:
+                    p1.setWeapon(Weapon.STICK);
+            }
+        }
+        p1 = new Player(name, 50, p1.getWeapon());
+        System.out.println("You player's stats are: " + p1.toString());
+    }
+}
