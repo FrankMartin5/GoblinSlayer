91494aced3d4add428a38dd4c20cc690f5d30bcc
Martin <cedmarth@bcd074432761.ant.amazon.com>
2022-05-16 09:07:27 -0700

    Started player Test setUp()

--- test/playerTest.java
+++ test/playerTest.java
@@ -1,10 +1,12 @@
 import com.goblinslayer.player.Player;
+import com.goblinslayer.player.Weapon;
 import org.junit.Before;
 
 public class playerTest {
     private Player player;
+    
     @Before
     public void setUp() {
-
+        player = new Player("p1", 50, Weapon.AXE, player.getPower());
     }
 }
